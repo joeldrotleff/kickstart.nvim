0892ab252561380d5fa85f2473e6be8b9a924c6a
Joel Drotleff <joel.drotleff@me.com>
2024-05-31 21:58:56 -0700

    switch to block in insert mode

--- init.lua
+++ init.lua
@@ -785,5 +785,8 @@ require('lspconfig').sourcekit.setup {
   cmd = { '/usr/local/bin/sourcekit-lsp' },
 }
 
+-- Use a block cursor in normal mode
+vim.opt.guicursor = ''
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

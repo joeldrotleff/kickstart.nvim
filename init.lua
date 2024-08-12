dbdc1f4972a1ac5795fb57dbd2ab7432f5ab330e
joeldrotleff <joel.drotleff@me.com>
2024-08-12 15:13:13 -0700

    fix specifying non-existant python version

--- init.lua
+++ init.lua
@@ -899,7 +899,7 @@ vim.api.nvim_set_hl(0, 'Cursor', { bg = 'red' })
 vim.api.nvim_set_option_value('guicursor', 'i:block-Cursor/lCursor', { scope = 'global' }) --
 
 -- Specify the path to the python interpreter because apparently it takes 1-2s to find it on startup :(
-vim.g.python3_host_prog = vim.fn.expand '~/.virtualenvs/debugpy/bin/python'
+vim.g.python3_host_prog = vim.fn.expand 'which python3'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

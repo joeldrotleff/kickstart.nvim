0facb7cbffbdbcf45b99e85f987fe80af87ef61b
Joel Drotleff <joel.drotleff@me.com>
2024-08-18 20:34:36 -0700

    harpoon auto saving

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,7 +1,12 @@
 local function config_harpoon()
   local harpoon = require 'harpoon'
 
-  harpoon:setup()
+  harpoon:setup {
+    settings = {
+      save_on_toggle = true,
+      sync_on_ui_close = true,
+    },
+  }
 
   vim.keymap.set('n', '<leader>pm', function()
     harpoon.ui:toggle_quick_menu(harpoon:list())

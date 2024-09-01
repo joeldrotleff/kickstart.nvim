176df908ea90d76a2940d2321a24b60734ebae99
Joel Drotleff <joel.drotleff@me.com>
2024-09-01 15:38:01 -0700

    disable swift treesitter

--- init.lua
+++ init.lua
@@ -767,6 +767,7 @@ require('lazy').setup({
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ignore_install = { 'swift' }, -- swift ts grammar causes issues
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {

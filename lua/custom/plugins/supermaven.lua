7cf024ae8da07c3324062b658438e5d8a452887f
Joel Drotleff <joel.drotleff@me.com>
2024-10-22 22:54:04 -0700

    switch to supermaven for code completion

---
+++ lua/custom/plugins/supermaven.lua
@@ -0,0 +1,10 @@
+return {
+  'supermaven-inc/supermaven-nvim',
+  opts = {
+    keymaps = {
+      accept_suggestion = '<M-l>',
+      clear_suggestion = '<M-x>',
+      accept_word = '<M-j>',
+    },
+  },
+}

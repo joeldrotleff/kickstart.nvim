6a4f315bebadf3e8b5f2d69ed367a33edcff79f6
joeldrotleff <joel.drotleff@me.com>
2024-10-23 12:10:19 -0700

    back to copilot until maven gets approved

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,10 +1,10 @@
 return {
-  'supermaven-inc/supermaven-nvim',
-  opts = {
-    keymaps = {
-      accept_suggestion = '<M-l>',
-      clear_suggestion = '<M-x>',
-      accept_word = '<M-j>',
-    },
-  },
+  -- 'supermaven-inc/supermaven-nvim',
+  -- opts = {
+  --   keymaps = {
+  --     accept_suggestion = '<M-l>',
+  --     clear_suggestion = '<M-x>',
+  --     accept_word = '<M-j>',
+  --   },
+  -- },
 }

f21bcbb8fbd401137b13e017b7fd71e576eafcb7
Joel Drotleff <joel.drotleff@me.com>
2024-12-09 09:23:15 -0800

    Uninstall mason

--- init.lua
+++ init.lua
@@ -461,10 +461,6 @@ require('lazy').setup({
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
@@ -603,90 +599,33 @@ require('lazy').setup({
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
       local lspconfig = require 'lspconfig'
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
+      -- This one still isn't brew installable, seems too fresh
+      -- ...maybe someday
+      -- lspconfig.fish_lsp.setup {}
 
-        -- Deno and Typescript LSPs can conflict, so use a root_dir to differentiate
-        denols = {
-          root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
-        },
-
-        jsonls = {},
-
-        tailwindcss = {},
-
-        -- TODO: Uncomment this once new version of Neovim that adds fish lsp support is released
-        -- fish_lsp = {},
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
+      lspconfig.pyright.setup {}
+      lspconfig.jsonls.setup {}
+      lspconfig.tailwindcss.setup {}
+      lspconfig.lua_ls.setup {
+        opts = {
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
-        },
-      }
-
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
-      require('mason').setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            lspconfig[server_name].setup(server)
-          end,
         },
       }
 
       -- Deno and Typescript LSPs can conflict, so use a root_dir to differentiate
       lspconfig.denols.setup {
         root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
+        cmd = { 'deno', 'lsp' },
       }
+
       lspconfig.ts_ls.setup {
-        root_dir = lspconfig.util.root_pattern 'package.json',
+        root_dir = lspconfig.util.root_pattern 'package.jsonnn',
         single_file_support = false,
       }
     end,

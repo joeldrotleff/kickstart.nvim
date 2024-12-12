01fe7f306cc77faa230e0382895b7d2e44bd077d
joeldrotleff <joel.drotleff@me.com>
2024-12-12 11:14:35 -0800

    use parent sdr directory

--- lua/custom/fish/scripts/new_daily_note.fish
+++ lua/custom/fish/scripts/new_daily_note.fish
@@ -43,7 +43,7 @@ end
 ln -f -s $new_file $base_dir/today.md
 
 # Change directory to ~/code/vibecheck
-cd ~/code/joya/sdr/client/
+cd ~/code/joya/sdr/
 
 # Open the new file in neovim
 nvim $new_file

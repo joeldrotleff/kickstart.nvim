b8773c076dbbb53534437fc5eb33e676c31dd51a
Joel Drotleff <joel.drotleff@me.com>
2025-01-01 22:54:43 -0800

    stop noice from messing with bottom messages

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,24 +1,17 @@
 local messagesToIgnore = { 'AutoSave', 'fewer line', 'more line', 'Hunk', 'change' }
 
-
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    routes = {
-      unpack(vim.tbl_map(function(pattern)
-        return {
-          filter = {
-            event = 'msg_show',
-            find = pattern,
-          },
-          opts = { skip = true },
-        }
-      end, messagesToIgnore))
+    messages = {
+      enabled = false,
+    },
+    notify = {
+      enabled = false,
     },
   },
   dependencies = {
     'MunifTanjim/nui.nvim',
-    'rcarriga/nvim-notify',
   },
 }

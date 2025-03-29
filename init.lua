30502b916bf93304e453198d411834af8b4032fb
Joel Drotleff <joel.drotleff@me.com>
2025-03-28 22:45:41 -0700

    no more - part of words

--- init.lua
+++ init.lua
@@ -1135,9 +1135,6 @@ vim.keymap.set('n', '<leader>lf', function()
   end
 end, { desc = 'Fix (Swift) LSP by re-running xcode build tool' })
 
--- Include - in 'words' since it gets used a lot in tailwind css
-vim.cmd 'set iskeyword+=-'
-
 -- vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})
 
 -- The line beneath this is called `modeline`. See `:help modeline`

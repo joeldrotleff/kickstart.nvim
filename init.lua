25755bcef9ec2a64ad7bde2256ef34eb857c5d62
Joel Drotleff <joel.drotleff@me.com>
2025-04-12 16:53:48 -0700

    Move settings to separate file

--- init.lua
+++ init.lua
@@ -1,103 +1,4 @@
--- Disable editorconfig support (because MP uses settings I don't like)
-vim.g.editorconfig = false
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-
--- Disable this for now as it's annoying for the Teams project
--- vim.opt.list = true
--- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = false
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- New virtual lines feature from Neovim 0.11
-vim.diagnostic.config {
-  -- virtual_lines = { enabled = true },
-  virtual_text = true,
-}
-
--- Import keymaps
-require 'keymaps'
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- Install Lazy.nvim for plugin management
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -107,6 +8,11 @@ if not vim.uv.fs_stat(lazypath) then
   end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
+
+
+require 'settings'
+require 'keymaps'
+
 
 require('lazy').setup {
   spec = {
@@ -128,102 +34,37 @@ require('lazy').setup {
       -- Main LSP Configuration
       'neovim/nvim-lspconfig',
       dependencies = {
-
         -- Useful status updates for LSP.
         -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
         { 'j-hui/fidget.nvim', opts = {} },
-
         -- Allows extra capabilities provided by nvim-cmp
         'hrsh7th/cmp-nvim-lsp',
       },
       config = function()
-        -- Brief aside: **What is LSP?**
-        --
-        -- LSP is an initialism you've probably heard, but might not understand what it is.
-        --
-        -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-        -- and language tooling communicate in a standardized fashion.
-        --
-        -- In general, you have a "server" which is some tool built to understand a particular
-        -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-        -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-        -- processes that communicate with some "client" - in this case, Neovim!
-        --
-        -- LSP provides Neovim with features like:
-        --  - Go to definition
-        --  - Find references
-        --  - Autocompletion
-        --  - Symbol Search
-        --  - and more!
-        --
-        -- Thus, Language Servers are external tools that must be installed separately from
-        -- Neovim. This is where `mason` and related plugins come into play.
-        --
-        -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-        -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-        --  This function gets run when an LSP attaches to a particular buffer.
-        --    That is to say, every time a new file is opened that is associated with
-        --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-        --    function will be executed to configure the current buffer
+        -- LSP Configuration
         vim.api.nvim_create_autocmd('LspAttach', {
           group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
           callback = function(event)
-            -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-            -- to define small helper and utility functions so you don't have to repeat yourself.
-            --
-            -- In this case, we create a function that lets us more easily define mappings specific
-            -- for LSP related items. It sets the mode, buffer and description for us each time.
             local map = function(keys, func, desc)
               vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
             end
 
             local builtin = require 'telescope.builtin'
-
-            -- Jump to the definition of the word under your cursor.
-            --  This is where a variable was first declared, or where a function is defined, etc.
-            --  To jump back, press <C-t>.
             map('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
-
             map('gD', function()
               builtin.lsp_definitions { jump_type = 'vsplit' }
             end, '[G]oto [Definition] in new split')
-
-            -- Find references for the word under your cursor.
             map('gr', builtin.lsp_references, '[G]oto [R]eferences')
-
-            -- Jump to the implementation of the word under your cursor.
-            --  Useful when your language has ways of declaring types without an actual implementation.
             map('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
-
-            -- Jump to the type of the word under your cursor.
-            --  Useful when you're not sure what type a variable is and you want to see
-            --  the definition of its *type*, not where it was *defined*.
             map('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
-
-            -- Fuzzy find all the symbols in your current document.
-            --  Symbols are things like variables, functions, types, etc.
             map('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-
-            -- Fuzzy find all the symbols in your current workspace.
-            --  Similar to document symbols, except searches over your entire project.
             map('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-            -- Rename the variable under your cursor.
-            --  Most Language Servers support renaming across files, etc.
             map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-            -- Execute a code action, usually your cursor needs to be on top of an error
-            -- or a suggestion from your LSP for this to activate.
             map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-            -- The following two autocommands are used to highlight references of the
-            -- word under your cursor when your cursor rests there for a little while.
-            --    See `:help CursorHold` for information about when this is executed
-            --
-            -- When you move your cursor, the highlights will be cleared (the second autocommand).
-            local client = vim.lsp.get_client_by_id(event.data.client_id)
-            if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+            local client_id = event.data.client_id
+            local client = client_id and vim.lsp.get_client_by_id(client_id)
+            if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
               local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
               vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                 buffer = event.buf,
@@ -246,10 +87,6 @@ require('lazy').setup {
               })
             end
 
-            -- The following code creates a keymap to toggle inlay hints in your
-            -- code, if the language server you are using supports them
-            --
-            -- This may be unwanted, since they displace some of your code
             if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
               map('<leader>th', function()
                 vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
@@ -258,17 +95,9 @@ require('lazy').setup {
           end,
         })
 
-        -- LSP servers and clients are able to communicate to each other what features they support.
-        --  By default, Neovim doesn't support everything that is in the LSP specification.
-        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
         local lspconfig = require 'lspconfig'
-
-        -- This one still isn't brew installable, seems too fresh
-        -- ...maybe someday
-        -- lspconfig.fish_lsp.setup {}
 
         lspconfig.pyright.setup {}
         lspconfig.jsonls.setup {}
@@ -285,7 +114,6 @@ require('lazy').setup {
           },
         }
 
-        -- Deno and Typescript LSPs can conflict, so use a root_dir to differentiate
         lspconfig.denols.setup {
           root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
           cmd = { 'deno', 'lsp' },
@@ -294,6 +122,10 @@ require('lazy').setup {
         lspconfig.ts_ls.setup {
           root_dir = lspconfig.util.root_pattern 'tsconfig.json',
           single_file_support = false,
+        }
+
+        lspconfig.sourcekit.setup {
+          cmd = { 'sourcekit-lsp' },
         }
       end,
     },
@@ -333,66 +165,27 @@ require('lazy').setup {
       source = '📄',
       start = '🚀',
       task = '📌',
-      lazy = '�� ',
+      lazy = '💤 ',
     },
   },
 }
 
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- Install this plugin "manually" so I can comment out easily the keymaps I don't want
 vim.cmd('source ' .. vim.fn.stdpath 'config' .. '/lua/custom/unimpaired.vim')
-
-local lspconfig = require 'lspconfig'
-lspconfig.sourcekit.setup {
-  cmd = { 'sourcekit-lsp' },
-
-  -- (This 'xcrun' technique seems to cause a slowdown on first startup, so I'm disabling it)
-  -- cmd = {
-  --   -- By using xcrun we make sure to use whichever version of the sourcekit-lsp tool is specified via xcode-select
-  --   vim.trim(vim.fn.system 'xcrun -f sourcekit-lsp'),
-  -- },
-}
-
--- Use a block cursor in normal mode
-vim.opt.guicursor = ''
-
-vim.opt.termguicolors = true
-
-vim.opt.linebreak = true
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-
--- Prevent transparency plugin from making the statusline transparent
-vim.g.exclude_groups = {
-  'StatusLine',
-  'StatusLineNC',
-}
-
--- Set the colorscheme
-vim.g.lumen_light_colorscheme = 'e-ink'
-vim.g.lumen_dark_colorscheme = 'embark'
-
-if vim.o.background == 'light' then
-  vim.cmd.colorscheme(vim.g.lumen_light_colorscheme)
-else
-  vim.cmd.colorscheme(vim.g.lumen_dark_colorscheme)
-end
-
--- Apparently I'm supposed to do this after setting colorscheme
-require('avante_lib').load()
-
--- Set cursor to a bright/highlight color when in insert mode
--- local wildmenu_hl = vim.api.nvim_get_hl_by_name('WildMenu', true).background
-vim.api.nvim_set_hl(0, 'Cursor', { bg = 'red' })
-
--- Make cursor a block in insert mode
-vim.api.nvim_set_option_value('guicursor', 'i:block-Cursor/lCursor', { scope = 'global' }) --
-
--- Specify the path to the python interpreter because apparently it takes 1-2s to find it on startup :(
-vim.g.python3_host_prog = vim.fn.expand 'which python3'
-
--- This needs to be called manually, it seems, even though LazyVim already does it
-require('breadcrumbs').setup()
 
 -- Fugitive and Gitsigns conflict when trying to use :G
 -- So define G explicitly to be :Git

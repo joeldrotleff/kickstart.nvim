600229c61f1989ba2b73febb582145b5e2227896
joeldrotleff <joel.drotleff@me.com>
2025-04-30 15:15:07 -0700

    Use latest chatgpt model
    fix

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -4,7 +4,7 @@ return {
   event = 'VeryLazy',
   opts = {
     openai_params = {
-      model = 'gpt-4o',
+      model = 'gpt-4.1-2025-04-14',
       max_tokens = 4096,
     },
     predefined_chat_gpt_prompts = '~/Documents/custom-chatgpt-prompts.csv',
@@ -16,5 +16,7 @@ return {
   dependencies = {
     'MunifTanjim/nui.nvim',
     'nvim-lua/plenary.nvim',
+    'folke/trouble.nvim', -- optional
+    'nvim-telescope/telescope.nvim',
   },
 }

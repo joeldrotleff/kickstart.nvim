5c664e81e602e0a3138b9f023ae128cbed9e2165
joeldrotleff <joel.drotleff@me.com>
2025-04-30 15:17:33 -0700

    Remove noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,20 +1,20 @@
 local messagesToIgnore = { 'AutoSave', 'fewer line', 'more line', 'Hunk', 'change' }
 
 return {
-  'folke/noice.nvim',
-  event = 'VeryLazy',
-  opts = {
-    messages = {
-      enabled = false,
-    },
-    notify = {
-      enabled = false,
-    },
-    presets = {
-      bottom_search = true,
-    },
-  },
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-  },
+  -- 'folke/noice.nvim',
+  -- event = 'VeryLazy',
+  -- opts = {
+  --   messages = {
+  --     enabled = false,
+  --   },
+  --   notify = {
+  --     enabled = false,
+  --   },
+  --   presets = {
+  --     bottom_search = true,
+  --   },
+  -- },
+  -- dependencies = {
+  --   'MunifTanjim/nui.nvim',
+  -- },
 }
